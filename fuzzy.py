afe924c28605866bb92b6de7f163666fff6203f7
R-PC\R <sk118420@gmail.com>
2018-11-25 18:45:00 +0600

    fuzzy llogic implemented with final output

--- fuzzy.py
+++ fuzzy.py
@@ -3,7 +3,6 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
 
-
 age = ctrl.Antecedent(np.arange(18, 66, 1), 'age')
 bad_words = ctrl.Antecedent(np.arange(1, 11, 1), 'word')
 output = ctrl.Consequent(np.arange(0, 4, 1), 'output')
@@ -29,23 +28,46 @@ rule9 = ctrl.Rule(age['high'] & bad_words['high'], output['high'])
 outputping_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9])
 outputping = ctrl.ControlSystemSimulation(outputping_ctrl)
 
-outputping.input['age'] = 22
-outputping.input['word'] = 1
-
-# Crunch the numbers
-outputping.compute()
-value=outputping.output['output']
-label=""
-if 0<=value and value<=1:
-    label="low"
-elif 1<=value and value<=2:
-    label="medium"
-elif 2<=value and value<=3:
-    label="high"
-print(value,label)
-
 # age.view()
 # bad_words.view()
 # output.view(sim=outputping)
 
 # plt.show()
+
+import csv
+
+bad_words = ["bitch", "slut", "brown", "black", "suck", "fuck", "boobs", "ass"]
+
+
+def count_bad_words(sentence):
+    count = 0
+    words = sentence.lower().split(" ")
+    for word in words:
+        if word in bad_words:
+            count += 1
+    return count
+
+
+outputs = []
+outputs += ["sentence,output,age,type"]
+with open('output.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        outputping.input['age'] = int(row['age'])
+        outputping.input['word'] = count_bad_words(row['Sentence'])
+
+        # Crunch the numbers
+        outputping.compute()
+        value = outputping.output['output']
+        label = ""
+        if 0 <= value <= 1:
+            label = "low"
+        elif 1 <= value <= 2:
+            label = "medium"
+        elif 2 <= value <= 3:
+            label = "high"
+        outputs += [row['Sentence'] + "," + row['output'] + "," + row["age"] + "," + label]
+file = open("output_final.csv", "w+")
+for line in outputs:
+    file.write(line + "\n")
+file.close()
--- main.py
+++ main.py
@@ -20,18 +20,20 @@ t = [({word: (word in word_tokenize(x[0])) for word in all_words}, x[1]) for x i
 # Train the classifier
 classifier = nltk.NaiveBayesClassifier.train(t)
 # Creating a test case
-outputs = [["Sentence,output," + ','.join(classes)]]
+outputs = [["Sentence,output," + ','.join(classes)+","+"age"]]
 with open('test.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         test_sentence = row['test']
+        test_age = row['age']
         test_sent_features = {word.lower(): (word in word_tokenize(test_sentence.lower())) for word in all_words}
         print(test_sentence + "," + classifier.classify(test_sent_features))
         percentage = {}
         for c in classes:
             percentage[c] = "{0:.2f}".format(classifier.prob_classify(test_sent_features).prob(c) * 100)
         outputs += [
-            [test_sentence + "," + classifier.classify(test_sent_features) + "," + ','.join(list(percentage.values()))]]
+            [test_sentence + "," + classifier.classify(test_sent_features) + "," + ','.join(
+                list(percentage.values())) + "," + test_age]]
 file = open("output.csv", "w+")
 for line in outputs:
     file.write(line[0] + "\n")
